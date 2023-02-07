65b4ef1b51b834ac47c43d937e8aebc84f757fd4
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-07 17:31:34 +0200

    added export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './Button';
 export * from './SelectComponent';
+export * from './DatePicker';
+export * from './MovieCard';
+export * from './SliderComponent';
+export * from './Tag';
