3824e78abbcf8f52a62c2821a0148947ca5ab9a8
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-08 09:42:45 +0200

    bugfix input

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react'
-import './Input.css'
+import React, { useState } from 'react';
+import './Input.css';
 
-export interface ButtonProps {
-  label?: string
-  onClick?: () => any
-  className?: string
-  [name: string]: any
-}
+// export interface ButtonProps {
+//   label?: string
+//   onClick?: () => any
+//   className?: string
+//   [name: string]: any
+// }
 // eslint-disable-next-line
-const Input = (props: ButtonProps) => {
-  const [value, setValue] = useState('')
+const Input = () => {
+  const [value, setValue] = useState('');
 
   return (
-    <div className='wrapperInput input-effect'>
+    <div className="wrapperInput input-effect">
       <input
         type={'text'}
-        className='mainInput'
+        className="mainInput"
         value={value}
-        placeholder=''
+        placeholder=""
         onChange={(e) => setValue(e.target.value)}
       />
       {value.length === 0 && <label>Search...</label>}
-      <span className='focus-border'>
+      <span className="focus-border">
         <i></i>
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
