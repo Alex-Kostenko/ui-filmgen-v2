97e564f79c11dae647c022c0169c268b8a054185
Misha Kompaniec <mikokompaniec@gmail.com>
2023-02-15 11:43:00 +0200

    plug card photo and new version

--- src/MovieCard/MovieCard.tsx
+++ src/MovieCard/MovieCard.tsx
@@ -19,12 +19,21 @@ const MovieCard = (props: MovieCardProps) => {
   return (
     <div className="wrapper">
       <div {...props} className={'movie-card-container ' + props.className}>
-        <img
-          className="movie-img"
-          onClick={props.action}
-          src={props.img}
-          alt="img"
-        />
+        {props.img ? (
+          <img
+            className="movie-img"
+            onClick={props.action}
+            src={props.img}
+            alt="img"
+          />
+        ) : (
+          <img
+            className="movie-img"
+            onClick={props.action}
+            src="https://media.istockphoto.com/id/1357365823/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=PM_optEhHBTZkuJQLlCjLz-v3zzxp-1mpNQZsdjrbns="
+            alt="img"
+          />
+        )}
         <div className="description-container">
           <div className="description-header">
             <div className="title-conteiner">
