4660d9d147e5a7a323365cadbd774bff78122a56
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-01 16:32:47 +0200

    props for input v42

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Input.css';
 
 export interface InputProps {
   label?: any;
-  onBlur: any;
+  [name: string]: any;
 }
 
 const Input = (props: InputProps) => {
-  const [value, setValue] = useState('');
-
   return (
     <div className="wrapperInput input-effect">
-      <input
-        type={'text'}
-        className="mainInput"
-        value={value}
-        placeholder=""
-        onChange={(e) => setValue(e.target.value)}
-        onBlur={props.onBlur}
-      />
-      {value.length === 0 && <label>{props.label}</label>}
+      <input {...props} type="text" className="mainInput" />
+      <label>{props.label}</label>
       <span className="focus-border">
         <i></i>
       </span>
