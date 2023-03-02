5680f0876ba965f93c4d1a40a41e0a558dfbc807
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-02 10:48:35 +0200

    label input v43

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -3,14 +3,19 @@ import './Input.css';
 
 export interface InputProps {
   label?: any;
+  value: any;
   [name: string]: any;
 }
 
 const Input = (props: InputProps) => {
   return (
     <div className="wrapperInput input-effect">
-      <input {...props} type="text" className="mainInput" />
-      <label>{props.label}</label>
+      <input {...props} type="text" className="mainInput" value={props.value} />
+      {props.value.length === 0 ? (
+        <label className="mainlabel">{props.label}</label>
+      ) : (
+        <label className="mainlabel secondMainlabel">{props.label}</label>
+      )}
       <span className="focus-border">
         <i></i>
       </span>
