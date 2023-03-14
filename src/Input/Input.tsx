d97fcd1f8e41a51a160f735a6c1d6da13c4cdbe6
alex-kostenko <0984181760ss@gmail.com>
2023-03-14 16:08:18 +0200

    update again

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,33 +1,19 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './Input.css';
 
 export interface InputProps {
   label?: any;
   value?: any;
-  searchTerm: string;
   [name: string]: any;
 }
 
 const Input = (props: InputProps) => {
-  const reff: any = useRef(null);
-
-  const { value = '', searchTerm = '' } = props;
+  const { value = '' } = props;
 
   return (
     <div className="wrapperInput input-effect">
-      <input
-        {...props}
-        type="text"
-        ref={reff}
-        className="mainInput"
-        defaultValue={searchTerm}
-        value={value}
-      />
-      <label
-        className={`mainlabel ${
-          value.length !== 0 && searchTerm !== '' && 'secondMainlabel'
-        }`}
-      >
+      <input {...props} type="text" className="mainInput" value={value} />
+      <label className={`mainlabel ${value.length !== 0 && 'secondMainlabel'}`}>
         {props.label}
       </label>
       <span className="focus-border">
