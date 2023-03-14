2a6d2985675dbba2808da07d42ab3f245f497b4c
alex-kostenko <0984181760ss@gmail.com>
2023-03-14 14:01:58 +0200

    add defaultValue for select

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Input.css';
 
 export interface InputProps {
   label?: any;
   value?: any;
+  searchTerm: string;
   [name: string]: any;
 }
 
 const Input = (props: InputProps) => {
+  const reff: any = useRef(null);
+
+  setTimeout(() => {
+    if (reff.current.defaultValue != null) {
+      reff.current.defaultValue = props.searchTerm;
+    }
+  }, 300);
+
   return (
     <div className="wrapperInput input-effect">
-      <input {...props} type="text" className="mainInput" value={props.value} />
-      {props.value.length === 0 ? (
+      <input
+        {...props}
+        type="text"
+        ref={reff}
+        className="mainInput"
+        value={props.value}
+      />
+      {props.value.length === 0 && props.searchTerm.length === 0 ? (
         <label className="mainlabel">{props.label}</label>
       ) : (
         <label className="mainlabel secondMainlabel">{props.label}</label>
