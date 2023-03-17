7ac1609841eb763f08633616b0da6aaea6892750
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-17 16:28:39 +0200

    changed tag  v56

--- src/Tag/Tag.tsx
+++ src/Tag/Tag.tsx
@@ -3,14 +3,20 @@ import './Tag.css';
 
 export interface TagProps {
   label?: any;
-  onClick?: () => any;
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
   className: string;
+  cross?: boolean;
 }
 
 const Tag = (props: TagProps) => {
   return (
-    <div {...props} className={props.className}>
-      <span onClick={props.onClick} className="tag_cross" />#{props.label}
+    <div className={props.className}>
+      {props.cross && (
+        <div onClick={props.onClick} className="tag_cross">
+          &#x2716;
+        </div>
+      )}
+      <span>#{props.label}</span>
     </div>
   );
 };
