6bf8f3f9d07f3173dcc21554f549db732f2ebffc
Misha Kompaniec <mikokompaniec@gmail.com>
2023-03-28 09:48:11 +0300

    change img movieCard v 59

--- src/MovieCard/MovieCard.tsx
+++ src/MovieCard/MovieCard.tsx
@@ -8,7 +8,7 @@ export interface MovieCardProps {
   subtitle: string;
   description: string;
   date: string;
-  img: string;
+  img: string | null;
   className?: string;
   labels: Array<string>;
   release: any;
@@ -20,17 +20,11 @@ const MovieCard = (props: MovieCardProps) => {
     <div className="wrapper">
       <div className={'movie-card-container ' + props.className}>
         {props.img ? (
-          <a
-            href={`/aboutFilm/${props.movieId}`}
-            style={{ textDecoration: 'none' }}
-          >
+          <a className="movie-link-img" href={`/aboutFilm/${props.movieId}`}>
             <img className="movie-img" src={props.img} alt="img" />
           </a>
         ) : (
-          <a
-            href={`/aboutFilm/${props.movieId}`}
-            style={{ textDecoration: 'none' }}
-          >
+          <a className="movie-link-img" href={`/aboutFilm/${props.movieId}`}>
             <img
               className="movie-img"
               src="https://media.istockphoto.com/id/1357365823/vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo.jpg?s=612x612&w=0&k=20&c=PM_optEhHBTZkuJQLlCjLz-v3zzxp-1mpNQZsdjrbns="
